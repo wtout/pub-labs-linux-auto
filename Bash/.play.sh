25b9cb4e99910fe21ebae41bdab9a00daba9df30
Wassim Tout <watout@cisco.com>
2024-03-20 11:10:15 -0400

    Updated the bash script to:   Ensure the passwords.yml file has 644 permissions   Optimize the mechanism to get the datacenter name   Change Ansible version to 8.7.0 Updated the ansible.cfg file to remove the deprecated gather_subset setting Updated the vars definition format at the task/block level to remove deprecated format in all the roles Updated the infra_dns_records role to optimize and improve structure Updated the vars definition format at the task/block level to remove deprecated format Updated the vm_fromiso role to correct a typo in task_update_disks_self_healing.yml

--- Bash/.play.sh
+++ Bash/.play.sh
@@ -380,7 +380,7 @@ function get_creds_prefix() {
     local DATACENTER
     local CREDS_PREFIX
 	[[ -f "${SYS_DEF}" ]] && FILETOCHECK="${SYS_DEF}" || FILETOCHECK="${SYS_ALL}"
-	DATACENTER=$(cat "${FILETOCHECK}" | sed "/^$/d" | grep -A32 -P "^datacenter:$" | sed -n "/${1}:/,+2p" | sed -n "/name:/,1p" | awk -F ': ' '{print $NF}' | sed "s/'//g")
+	DATACENTER=$(cat "${FILETOCHECK}" | sed "/^\s*$/d" | grep -A22 -P "^datacenter:$" | sed -n "/${1}:/,+2p" | sed -n "/name:/,1p" | awk -F ': ' '{print $NF}' | sed "s/'//g")
 	if [[ "${?}" == "0" ]] && [[ "${DATACENTER}" != "" ]] && [[ "${DATACENTER}" != "''" ]]
 	then
 		case ${DATACENTER} in
@@ -597,7 +597,7 @@ function check_updates() {
 						[[ ${?} != 0 ]] && echo -e "\nThe installation package update has failed with the following error:\n\n${BOLD}$(cat "${PWD}"/.pullerr)${NORMAL}\n\n" && EC='exit'
 						rm -f "${PWD}"/.pullerr
 					else
-						EC='continue'
+						EC=':'
 					fi
 				fi
 				${EC}
@@ -722,7 +722,7 @@ ANSIBLE_CFG="ansible.cfg"
 ANSIBLE_LOG_LOCATION="Logs"
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
-ANSIBLE_VERSION='7.7.0'
+ANSIBLE_VERSION='8.7.0'
 ANSIBLE_VARS="vars/datacenters.yml"
 PASSVAULT="vars/passwords.yml"
 REPOVAULT="vars/.repovault.yml"
@@ -758,6 +758,7 @@ check_container && stop_container
 image_prune
 start_container
 [[ $- =~ x ]] && debug=1 && [[ "${SECON}" == "true" ]] && set +x
+chmod 644 "${PASSVAULT}"
 PCREDS_LIST=$(get_creds primary)
 SCREDS_LIST=$(get_creds secondary)
 PROXY_ADDRESS=$(get_proxy) || PA=${?}
